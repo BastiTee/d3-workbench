e53b8960c267a83b5533b93944ade5e440244304
Basti Tee <basti.tee@posteo.de>
2017-08-18 17:48:57 +0200

    better builder;

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -5,13 +5,12 @@ cd $( dirname $( readlink -f $0 ))/../
 [ "$1" == "-f" ] && force=1 || force=0
 
 echo "-- preparing environment..."
-sudo npm install -g uglify-es
+[ -z $( command -v uglifyjs ) ] && sudo npm install -g uglify-es
 [ ! -z "$( git diff-index --name-only HEAD -- )" ] && [ $force -eq 0 ] && {
     echo "You have uncommitted changes."
     echo "Will abort unless running with '-f' force-option."
     exit 1
 }
-mkdir -p _release/
 
 echo "-- creating version string"
 curr_module_version=$( head -n5 package.json | grep version |\
@@ -19,6 +18,8 @@ awk '{print $2}' | tr -d "\"" | tr -d , )
 curr_commit_hash=$( git rev-parse --short HEAD )
 curr_ver="${curr_module_version}.${curr_commit_hash}"
 echo "-- version is: ${curr_ver}"
+tdir="d3-wb-releases/${curr_ver}"
+mkdir -p $tdir
 
 infiles=$(
 {
@@ -33,17 +34,16 @@ find ./d3-wb/reusable-charts/*.js  -maxdepth 1
 echo -e "-- input-files: \n\n$( echo $infiles | tr " " "\n" )\n"
 
 echo "-- packing human-readable js..."
-cat $infiles > _release/d3-wb.${curr_ver}.js
+cat $infiles > ${tdir}/d3-wb.js
 
 echo "-- uglifying human-readable js..."
-uglifyjs --compress --mangle -- $infiles \
-> _release/d3-wb.${curr_ver}.min.js
+uglifyjs --compress --mangle -- $infiles > ${tdir}/d3-wb.min.js
 
 echo -e "-- result:\n"
 
-before_bytes=$( wc -c _release/d3-wb.${curr_ver}.js |\
+before_bytes=$( wc -c ${tdir}/d3-wb.js |\
 tail -n1 | cut -d" " -f1 )
-after_bytes=$( wc -c _release/d3-wb.${curr_ver}.min.js |\
+after_bytes=$( wc -c ${tdir}/d3-wb.min.js |\
 tail -n1 | cut -d" " -f1 )
 echo "js:  $before_bytes > $after_bytes bytes (-$( echo "scale=2;(1-$after_bytes/$before_bytes)*100" | bc )%)"
 
