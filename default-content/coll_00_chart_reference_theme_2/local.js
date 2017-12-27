1eace9a909ced3e3c3b56f27bc5a18a5caab16b9
Basti Tee <basti.tee@posteo.de>
2017-12-27 21:43:07 +0100

    reapplied themes in default content;

---
+++ default-content/coll_00_chart_reference_theme_2/local.js
@@ -0,0 +1,27 @@
+(function() {
+    d3wb.theme('dark');
+    d3wb.util.injectCSS(`
+    * {
+        background-color: ` + d3wb.color.background + `;
+    }
+    .page-footer {
+        display: none;
+    }
+    .page-header-text {
+        color: ` + d3wb.color.foreground + ` !important;
+    }
+    .page-header img {
+        display: none;
+    }
+    
+    .visualization-title {
+        background-color: ` + d3wb.color.background + ` !important;
+        color: ` + d3wb.color.foreground + `;
+    }
+    
+    .page-button {
+        background-color: ` + d3wb.color.background + ` !important;
+        color: ` + d3wb.color.foreground + ` !important;
+    }
+    `)
+})();
