78d69e9cb514de1219e6106af589eaa5b46485b9
Basti Tee <basti.tee@posteo.de>
2018-01-02 16:18:18 +0100

    upgraded d3-sankey dep to latest 0.7.1 applying all necessary api changes;

--- d3-wb/d3-wb-reusable-charts/d3-wb-sankey.js
+++ d3-wb/d3-wb-reusable-charts/d3-wb-sankey.js
@@ -15,102 +15,72 @@ function wbSankeyDiagram() {
         selection.each(function(data, i, nodes) {
             let s = d3.select(nodes[i]);
 
-            let formatNumber = d3.format(',.0f');
-            let format = function(d) {
-                return formatNumber(d) + ' TWh';
-            };
-
             let sankey = d3.sankey()
                 .nodeWidth(15)
                 .nodePadding(10)
                 .size([width, height]);
 
-            let path = sankey.link();
-            sankey
-                .nodes(data.nodes)
-                .links(data.links)
-                .layout(32);
+            sankey(data);
 
-            let link = s.selectAll('.sankey-link')
-                .data(data.links)
-                .enter().append('path')
+            s.append('g')
                 .attr('class', 'sankey-link')
                 .style('fill', 'none')
                 .style('stroke', fill)
                 .style('stroke-opacity', .2)
-                .attr('d', path)
-                .style('stroke-width', function(d) {
-                    return Math.max(1, d.dy);
-                })
-                .sort(function(a, b) {
-                    return b.dy - a.dy;
+                .selectAll('path')
+                .data(data.links)
+                .enter().append('path')
+                .attr('d', d3.sankeyLinkHorizontal())
+                .attr('stroke-width', function(d) {
+                    return Math.max(1, d.width);
                 });
 
-            link.append('title')
-                .text(function(d) {
-                    return d.source.name + ' → ' +
-                    d.target.name + '\n' + format(d.value);
-                });
-
-            let dragmove = function(d, i, nodes) {
-                d3.select(nodes[i])
-                    .attr('transform', 'translate(' + d.x
-                    + ',' + (d.y = Math.max(0,
-                        Math.min(height - d.dy, d3.event.y))) + ')');
-                sankey.relayout();
-                link.attr('d', path);
-            };
-
-            let node = s.selectAll('.sankey-node')
-                .data(data.nodes)
-                .enter().append('g')
+            let node = s.append('g')
                 .attr('class', 'sankey-node')
-                .attr('transform', function(d) {
-                    return 'translate(' + d.x + ',' + d.y + ')';
-                })
-                .call(d3.drag()
-                    .subject(function(d) {
-                        return d;
-                    })
-                    .on('start', function(d, i, nodes) {
-                        nodes[i].parentNode.appendChild(nodes[i]);
-                    })
-                    .on('drag', dragmove));
+                .selectAll('g')
+                .data(data.nodes)
+                .enter().append('g');
 
             node.append('rect')
-                .style('cursor', 'move')
-                .style('shape-rendering', 'crispEdges')
-                .attr('height', function(d) {
-                    return d.dy;
+                .attr('x', function(d) {
+                    return d.x0;
                 })
-                .attr('width', sankey.nodeWidth())
-                .style('fill', function(d) {
-                    return d.color = colors(d.name.replace(/ .*/, ''));
+                .attr('y', function(d) {
+                    return d.y0;
+                })
+                .attr('height', function(d) {
+                    return d.y1 - d.y0;
+                })
+                .attr('width', function(d) {
+                    return d.x1 - d.x0;
+                })
+                .attr('fill', function(d) {
+                    return colors(d.name.replace(/ .*/, ''));
                 })
                 .style('stroke', function(d) {
                     return fill;
-                })
-                .append('title')
-                .text(function(d) {
-                    return d.name + '\n' + format(d.value);
                 });
 
             node.append('text')
-                .attr('x', -6)
-                .attr('y', function(d) {
-                    return d.dy / 2;
+                .style('fill', function(d) {
+                    return fill;
                 })
-                .attr('dy', '.35em')
+                .attr('x', function(d) {
+                    return d.x0 - 6;
+                })
+                .attr('y', function(d) {
+                    return (d.y1 + d.y0) / 2;
+                })
                 .attr('text-anchor', 'end')
-                .attr('fill', fill)
-                .attr('transform', null)
                 .text(function(d) {
                     return d.name;
                 })
                 .filter(function(d) {
-                    return d.x < width / 2;
+                    return d.x0 < width / 2;
                 })
-                .attr('x', 6 + sankey.nodeWidth())
+                .attr('x', function(d) {
+                    return d.x1 + 6;
+                })
                 .attr('text-anchor', 'start');
         });
     };
@@ -140,4 +110,4 @@ function wbSankeyDiagram() {
     };
 
     return chart;
-}
+};
